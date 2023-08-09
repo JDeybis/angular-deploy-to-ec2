69f5ad4d595edda98775574d46fe9ca630183004
Jhon Deybis <jhon.ichaccaya.27@unsch.edu.pe>
2023-08-09 00:45:04 -0500

    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-to-ec2';
+  title = 'Deploy to EC2 successfully';
 }
